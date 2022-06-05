a51146a9f79bd431a773a66f5e7f3e9823f5ed70
armstrony <haficahjogja@gmail.com>
2022-06-05 20:12:24 +0700

    Tambah layout

---
+++ app/src/main/java/com/example/mefora/HomePatientActivity.kt
@@ -0,0 +1,36 @@
+package com.example.mefora
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.mefora.databinding.ActivityHomePatientBinding
+
+class HomePatientActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomePatientBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar?.hide()
+        binding = ActivityHomePatientBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_activity_home_patient)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_food, R.id.navigation_doctor ,R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
